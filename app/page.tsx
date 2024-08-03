58b0bb561b8eeb9dac54bd5c0dc4d733f1b8fe3a
mohamedmaher <mohamedmaher.mm330@gmail.com>
2024-08-03 11:54:47 +0300

    center siginUp,sigin-in and create middleware and protect home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex items-center h-screen justify-center">
-      <div className="flex flex-col gap-2"></div>
+      <div className="flex flex-col gap-2">
+        <Button>Join</Button>
+      </div>
     </main>
   );
 }
